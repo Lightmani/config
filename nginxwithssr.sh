54aa31d7774f29cc7dacb089fc4d3c0596eb7899
Lightmani <43240742+Lightmani@users.noreply.github.com>
2020-06-03 15:49:59 +0800

    Update nginxwithssr.sh

--- nginxwithssr.sh
+++ nginxwithssr.sh
@@ -19,15 +19,15 @@ wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh
 apt install ufw -y
 ufw default allow
 ufw default deny
-ufw allow 28913
+ufw allow 22
 ufw allow 443
 ufw allow 80
 ufw enable
 
 
 
-wget http://soft.vpser.net/lnmp/lnmp1.6.tar.gz -cO lnmp1.6.tar.gz && tar zxf lnmp1.6.tar.gz && cd lnmp1.6
-./install.sh nginx
+#wget http://soft.vpser.net/lnmp/lnmp1.6.tar.gz -cO lnmp1.6.tar.gz && tar zxf lnmp1.6.tar.gz && cd lnmp1.6
+#./install.sh nginx
 
 #rng
 echo "HRNGDEVICE=/dev/urandom">>/etc/default/rng-tools
@@ -36,14 +36,17 @@ echo "HRNGDEVICE=/dev/urandom">>/etc/default/rng-tools
 
 
 cd /srv
-wget https://github.com/zhangxiang958/Tour4U/archive/dev.zip
-unzip dev.zip
-lnmp vhost add
-
+#wget https://github.com/zhangxiang958/Tour4U/archive/dev.zip
+#unzip dev.zip
+#lnmp vhost add
+wget https://github.com/ginuerzh/gost/releases/download/v2.11.1/gost-linux-amd64-2.11.1.gz
+gzip -d gost-linux-amd64-2.11.1.gz
+mv gost-linux-amd64-2.11.1.gz gost
+chmod +x gost
 
 wget https://raw.githubusercontent.com/Lightmani/shadowsocks_install/master/shadowsocksR.sh
 chmod +x shadowsocksR.sh
 bash shadowsocksR.sh
 
-echo "/usr/local/nginx/conf/nginx.conf "
-vi /usr/local/nginx/conf/nginx.conf 
+#echo "/usr/local/nginx/conf/nginx.conf "
+#vi /usr/local/nginx/conf/nginx.conf 
