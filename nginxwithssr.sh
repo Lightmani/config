505e5aa82caefa7026a953d3de98ac7e0b579f60
Lightmani <43240742+Lightmani@users.noreply.github.com>
2020-06-03 16:18:25 +0800

    Update nginxwithssr.sh

--- nginxwithssr.sh
+++ nginxwithssr.sh
@@ -28,6 +28,8 @@ ufw enable
 
 #wget http://soft.vpser.net/lnmp/lnmp1.6.tar.gz -cO lnmp1.6.tar.gz && tar zxf lnmp1.6.tar.gz && cd lnmp1.6
 #./install.sh nginx
+apt install nginx
+service nginx start
 
 #rng
 echo "HRNGDEVICE=/dev/urandom">>/etc/default/rng-tools
@@ -35,18 +37,24 @@ echo "HRNGDEVICE=/dev/urandom">>/etc/default/rng-tools
 
 
 
-cd /srv
 #wget https://github.com/zhangxiang958/Tour4U/archive/dev.zip
 #unzip dev.zip
 #lnmp vhost add
+cd /srv
 wget https://github.com/ginuerzh/gost/releases/download/v2.11.1/gost-linux-amd64-2.11.1.gz
 gzip -d gost-linux-amd64-2.11.1.gz
-mv gost-linux-amd64-2.11.1.gz gost
+mv gost-linux-amd64-2.11.1 gost
 chmod +x gost
+wget https://raw.githubusercontent.com/Lightmani/config/master/gost.service
+mv gost.service /etc/systemd/system/
+
+
+
 
 wget https://raw.githubusercontent.com/Lightmani/shadowsocks_install/master/shadowsocksR.sh
 chmod +x shadowsocksR.sh
 bash shadowsocksR.sh
+service gost start
 
 #echo "/usr/local/nginx/conf/nginx.conf "
 #vi /usr/local/nginx/conf/nginx.conf 
