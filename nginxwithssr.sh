cfaaac8d2862910729d1b2b936b870d12acf1e3b
Lightmani <43240742+Lightmani@users.noreply.github.com>
2020-06-09 09:21:22 +0800

    Update nginxwithssr.sh

--- nginxwithssr.sh
+++ nginxwithssr.sh
@@ -26,8 +26,7 @@ ufw enable
 
 
 
-#wget http://soft.vpser.net/lnmp/lnmp1.6.tar.gz -cO lnmp1.6.tar.gz && tar zxf lnmp1.6.tar.gz && cd lnmp1.6
-#./install.sh nginx
+#wget http://soft.vpser.net/lnmp/lnmp1.7.tar.gz -cO lnmp1.7.tar.gz && tar zxf lnmp1.7.tar.gz && cd lnmp1.7 && ./install.sh nginx
 apt install nginx
 service nginx start
 
